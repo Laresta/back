b461a79cdbbe07a05f8e3688163ec6433179f222
Laresta <duartebryan@hotmail.fr>
2023-01-16 21:11:09 +0100

    ah

--- Vue/menu.php
+++ Vue/menu.php
@@ -6,7 +6,7 @@ if ($_SESSION["droits"] == 2 ){
         <li><a href="../index.php?action=deconnecter"> Se deconnecter</a></li>
     </ul>
 <?php}
-else ($_SESSION["droits"] == 1)
+elseif ($_SESSION["droits"] == 1)
 {?>
     <ul>
         <li><a href="../index.php?action=listage_user"> Lister mes collections</a></li>
